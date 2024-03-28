05590aa11fe3e2bd4088bdf85bfdc7f724d976c2
Echava <edwinchavarriac@gmail.com>
2024-03-27 23:44:30 -0600

    cambio de tabla distrito

--- SQL/sc502_proyecto.sql
+++ SQL/sc502_proyecto.sql
@@ -1,82 +1,39 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 24-02-2024 a las 17:14:21
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `sc502_proyecto`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `almacenes`
---
 
 CREATE TABLE `almacenes` (
   `id_almacen` int(11) NOT NULL,
   `ubicacion` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cantones`
---
-
 CREATE TABLE `cantones` (
   `id_canton` int(11) NOT NULL,
   `nombre` varchar(25) NOT NULL,
-  `provicia_id` int(11) NOT NULL
+  `id_provincia` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `clientes`
---
 
 CREATE TABLE `clientes` (
   `id_cliente` int(11) NOT NULL,
   `nombre` varchar(25) NOT NULL,
   `apellido` varchar(25) NOT NULL,
-  `referencia_id` int(11) NOT NULL,
+  `id_distrito` int(11) NOT NULL,
   `telefono` varchar(25) NOT NULL,
   `email` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `compras`
---
-
 CREATE TABLE `compras` (
   `id_compra` int(11) NOT NULL,
-  `Proveedor_id` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
   `fecha date` date NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `detalles_facturas`
---
 
 CREATE TABLE `detalles_facturas` (
   `id_detalle number` int(11) NOT NULL,
@@ -86,12 +43,6 @@ CREATE TABLE `detalles_facturas` (
   `precio_unitario` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `detalle_compras`
---
-
 CREATE TABLE `detalle_compras` (
   `id_detalle` int(11) NOT NULL,
   `compra_id` int(11) NOT NULL,
@@ -100,23 +51,12 @@ CREATE TABLE `detalle_compras` (
   `precio_unitario` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `distritos`
---
-
 CREATE TABLE `distritos` (
-  `id_distritos` int(11) NOT NULL,
+  `id_distrito` int(11) NOT NULL,
   `nombre` varchar(25) NOT NULL,
+  `direccion` varchar(300) NOT NULL,
   `canton_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `facturaciones`
---
 
 CREATE TABLE `facturaciones` (
   `id_factura` int(11) NOT NULL,
@@ -126,12 +66,6 @@ CREATE TABLE `facturaciones` (
   `Estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `inventarios`
---
-
 CREATE TABLE `inventarios` (
   `id_inventario` int(11) NOT NULL,
   `producto_id` int(11) NOT NULL,
@@ -140,25 +74,14 @@ CREATE TABLE `inventarios` (
   `Cantidad_disponible` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos`
---
-
 CREATE TABLE `productos` (
   `id_producto` int(11) NOT NULL,
+  `id_proveedor` int(11) NOT NULL,
   `codigo` varchar(10) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `descripcion` varchar(255) NOT NULL,
   `precio` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `proveedores`
---
 
 CREATE TABLE `proveedores` (
   `id_proveedor` int(11) NOT NULL,
@@ -168,267 +91,163 @@ CREATE TABLE `proveedores` (
   `email` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `provincias`
---
-
 CREATE TABLE `provincias` (
   `id_provincia` int(11) NOT NULL,
   `nombre` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- --------------------------------------------------------
+CREATE TABLE `rol` (
+  `id_rol` int(11) NOT NULL,
+  `descripcion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Estructura de tabla para la tabla `referencias`
---
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `id_rol` int(11) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `usuario` varchar(50) DEFAULT NULL,
+  `contrasenna` varchar(255) DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
+  `activo` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `referencias` (
-  `id_referencia` int(11) NOT NULL,
-  `nombre` varchar(25) NOT NULL,
-  `distrito_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `almacenes`
---
 ALTER TABLE `almacenes`
   ADD PRIMARY KEY (`id_almacen`);
 
---
--- Indices de la tabla `cantones`
---
 ALTER TABLE `cantones`
   ADD PRIMARY KEY (`id_canton`),
-  ADD KEY `provicia_id` (`provicia_id`),
-  ADD KEY `provicia_id_2` (`provicia_id`);
+  ADD KEY `id_provincia` (`id_provincia`);
 
---
--- Indices de la tabla `clientes`
---
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id_cliente`),
-  ADD KEY `referencia_id` (`referencia_id`);
+  ADD KEY `id_distrito` (`id_distrito`);
 
---
--- Indices de la tabla `compras`
---
 ALTER TABLE `compras`
   ADD PRIMARY KEY (`id_compra`),
-  ADD KEY `Proveedor_id` (`Proveedor_id`);
+  ADD KEY `id_producto` (`id_producto`) USING BTREE;
 
---
--- Indices de la tabla `detalles_facturas`
---
 ALTER TABLE `detalles_facturas`
   ADD PRIMARY KEY (`id_detalle number`),
   ADD KEY `producto_id` (`producto_id`),
   ADD KEY `factura_id number` (`factura_id number`);
 
---
--- Indices de la tabla `detalle_compras`
---
 ALTER TABLE `detalle_compras`
   ADD PRIMARY KEY (`id_detalle`,`compra_id`),
   ADD KEY `producto_id` (`producto_id`),
   ADD KEY `compra_id` (`compra_id`);
 
---
--- Indices de la tabla `distritos`
---
 ALTER TABLE `distritos`
-  ADD PRIMARY KEY (`id_distritos`),
+  ADD PRIMARY KEY (`id_distrito`),
   ADD KEY `canton_id` (`canton_id`);
 
---
--- Indices de la tabla `facturaciones`
---
 ALTER TABLE `facturaciones`
   ADD PRIMARY KEY (`id_factura`),
   ADD KEY `cliente_id` (`cliente_id`);
 
---
--- Indices de la tabla `inventarios`
---
 ALTER TABLE `inventarios`
   ADD PRIMARY KEY (`id_inventario`),
   ADD KEY `producto_id` (`producto_id`),
   ADD KEY `almacen_id` (`almacen_id`);
 
---
--- Indices de la tabla `productos`
---
 ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id_producto`);
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_proveedor` (`id_proveedor`);
 
---
--- Indices de la tabla `proveedores`
---
 ALTER TABLE `proveedores`
   ADD PRIMARY KEY (`id_proveedor`);
 
---
--- Indices de la tabla `provincias`
---
 ALTER TABLE `provincias`
   ADD PRIMARY KEY (`id_provincia`);
 
---
--- Indices de la tabla `referencias`
---
-ALTER TABLE `referencias`
-  ADD PRIMARY KEY (`id_referencia`),
-  ADD KEY `distrito_id` (`distrito_id`);
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id_rol`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `usuario` (`usuario`),
+  ADD KEY `id_rol` (`id_rol`);
 
---
--- AUTO_INCREMENT de la tabla `almacenes`
---
+
 ALTER TABLE `almacenes`
   MODIFY `id_almacen` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `cantones`
---
 ALTER TABLE `cantones`
   MODIFY `id_canton` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `clientes`
---
 ALTER TABLE `clientes`
   MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `compras`
---
 ALTER TABLE `compras`
   MODIFY `id_compra` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `detalles_facturas`
---
 ALTER TABLE `detalles_facturas`
   MODIFY `id_detalle number` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `detalle_compras`
---
 ALTER TABLE `detalle_compras`
   MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `distritos`
---
 ALTER TABLE `distritos`
-  MODIFY `id_distritos` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_distrito` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `facturaciones`
---
 ALTER TABLE `facturaciones`
   MODIFY `id_factura` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `inventarios`
---
 ALTER TABLE `inventarios`
   MODIFY `id_inventario` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `productos`
---
 ALTER TABLE `productos`
   MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `proveedores`
---
 ALTER TABLE `proveedores`
   MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `provincias`
---
 ALTER TABLE `provincias`
-  MODIFY `id_provincia` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_provincia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
---
--- AUTO_INCREMENT de la tabla `referencias`
---
-ALTER TABLE `referencias`
-  MODIFY `id_referencia` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `rol`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- Restricciones para tablas volcadas
---
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- Filtros para la tabla `cantones`
---
+
 ALTER TABLE `cantones`
-  ADD CONSTRAINT `cantones_ibfk_1` FOREIGN KEY (`provicia_id`) REFERENCES `provincias` (`id_provincia`);
+  ADD CONSTRAINT `cantones_ibfk_1` FOREIGN KEY (`id_provincia`) REFERENCES `provincias` (`id_provincia`);
 
---
--- Filtros para la tabla `clientes`
---
 ALTER TABLE `clientes`
-  ADD CONSTRAINT `clientes_ibfk_1` FOREIGN KEY (`referencia_id`) REFERENCES `referencias` (`id_referencia`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `clientes_ibfk_1` FOREIGN KEY (`id_distrito`) REFERENCES `distritos` (`id_distrito`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Filtros para la tabla `compras`
---
 ALTER TABLE `compras`
-  ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`Proveedor_id`) REFERENCES `productos` (`id_producto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Filtros para la tabla `detalles_facturas`
---
 ALTER TABLE `detalles_facturas`
   ADD CONSTRAINT `detalles_facturas_ibfk_1` FOREIGN KEY (`factura_id number`) REFERENCES `facturaciones` (`id_factura`),
   ADD CONSTRAINT `detalles_facturas_ibfk_2` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id_producto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Filtros para la tabla `detalle_compras`
---
 ALTER TABLE `detalle_compras`
   ADD CONSTRAINT `detalle_compras_ibfk_1` FOREIGN KEY (`compra_id`) REFERENCES `compras` (`id_compra`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Filtros para la tabla `distritos`
---
 ALTER TABLE `distritos`
   ADD CONSTRAINT `distritos_ibfk_1` FOREIGN KEY (`canton_id`) REFERENCES `cantones` (`id_canton`);
 
---
--- Filtros para la tabla `facturaciones`
---
 ALTER TABLE `facturaciones`
   ADD CONSTRAINT `facturaciones_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id_cliente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Filtros para la tabla `inventarios`
---
 ALTER TABLE `inventarios`
   ADD CONSTRAINT `inventarios_ibfk_1` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id_producto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `inventarios_ibfk_2` FOREIGN KEY (`almacen_id`) REFERENCES `almacenes` (`id_almacen`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Filtros para la tabla `referencias`
---
-ALTER TABLE `referencias`
-  ADD CONSTRAINT `referencias_ibfk_1` FOREIGN KEY (`distrito_id`) REFERENCES `distritos` (`id_distritos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id_proveedor`);
+
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
