75885cd5fdc1f791eab91f798984f8123d904cd6
JoseDRC <106029826+JoseDRC@users.noreply.github.com>
2024-03-31 13:51:01 -0600

    productos

---
+++ SECTIONS/productos.php
@@ -0,0 +1,83 @@
+<?php
+require_once "../DAL/funciones_productos.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    require_once "../INCLUDE/head.php";
+    ?>
+</head>
+
+<body>
+    <?php
+    include "../INCLUDE/nav.php";
+    ?>
+    <div class="container">
+        <div class="row">
+            <h3>PRODUCTOS</h3>
+        </div>
+        <div class="row">
+            <p>
+                <?php
+                echo '<button type="button" class="btn btn-success" data-toggle="modal" 
+                data-target="#insertar">
+                <i class="fa fa-edit ">Nuevo producto</i></button>';
+                require "PRODUCTOS/insertar_productos.php";
+                ?>
+            </p>
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>Id Producto</th>
+                        <th>Nombre</th>
+                        <th>Inventario</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php
+                    $result = getProducto();
+                    if ($result->num_rows > 0) {
+                        foreach ($result as $row) {
+                            echo '<tr>';
+                            echo '<td>' . $row['id_productos'] . '</td>';
+                            echo '<td>' . $row['nombre'] . '</td>';
+                            echo '<td>' . getInventarios($row['inventarios_id']) . '</td>';
+                            echo '<td width=250>';
+                            echo '<button type="button" class="btn btn-primary" data-toggle="modal" 
+                            data-target="#ver' . $row['id_productos'] . ' ">
+                            <i class="fa fa-edit ">Ver</i></button>';
+                            echo ' ';
+                            echo '<button type="button" class="btn btn-success" data-toggle="modal" 
+                            data-target="#editar' . $row['id_productos'] . ' ">
+                            <i class="fa fa-edit ">Actualizar</i></button>';
+                            echo ' ';
+                            echo '<button type="button" class="btn btn-danger" data-toggle="modal" 
+                            data-target="#borrar' . $row['id_productos'] . ' ">
+                            <i class="fa fa-edit ">Borrar</i></button>';
+                            echo ' ';
+                            echo '</td>';
+                            require "PRODUCTOS/editar_productos.php";
+                            require "PRODUCTOS/ver_productos.php";
+                            require "PRODUCTOS/borrar_productos.php";
+                            echo '</tr>';
+                        }
+                    } else {
+                        echo "No hay datos";
+                    }
+                    Desconectar();
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div> <!-- /container -->
+
+    <?php
+    include "../INCLUDE/footer.php";
+    ?>
+</body>
+
+</html>
